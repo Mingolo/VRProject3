38b7b9e2e02e55ca2defb89f462155052df197f7
Mingolo <mingolonio@yahoo.com>
2016-03-17 01:31:26 -0400

    Navigation system is done.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public GameObject head;
+	public AudioSource teleportSound;
+	public ParticleSystem teleportParticles;
+	public int walkRange;
+
+	private int groundMask;
+	private Ray moveRay;
+	private RaycastHit walkHit;
+
+	// Use this for initialization
+	void Start () 
+	{
+		groundMask = LayerMask.GetMask ("Ground");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetButton ("Fire1"))
+		{
+			Move();
+		}
+	}
+
+	private void Move()
+	{		
+		moveRay.origin = head.transform.position;
+		moveRay.direction = head.transform.forward;
+
+		if (Physics.Raycast (moveRay, out walkHit, walkRange, groundMask)) 
+		{
+			teleportParticles.Play();
+			teleportSound.Play();
+			transform.position = walkHit.point;
+		}
+	}
+}
