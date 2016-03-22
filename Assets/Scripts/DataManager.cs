6db4528871bb3e570ce62b3fc970ffbc4b58a946
Stevie Carnell <scarnell@ufl.edu>
2016-03-21 22:51:37 -0400

    adding resources/scene for theaters, start of data input

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataManager : MonoBehaviour {
+
+	public int currentDecade;
+	public string currentDataset;
+	public string[] currentGroups;
+	public int lastDecade;
+	public string lastDataset;
+
+	public string[] raceGroups = { "White", "Black", "Hispanic", "Middle Eastern", "Asian", "Multi-racial" };
+	public string[] orientationGroups = {"Homosexual","Heterosexual","Bisexual" };
+
+	// Use this for initialization
+	void Start () {
+		currentDecade = 1930;
+		currentDataset = "Race";
+		currentGroups = raceGroups;
+
+		lastDecade = currentDecade;
+		lastDataset = currentDataset;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
